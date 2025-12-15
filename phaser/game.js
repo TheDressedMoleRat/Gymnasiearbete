cfcf669e9d1dc0892dfbff0fd491760fdaffe3a1
TheDressedMoleRat <alvardepaulis@gmail.com>
2025-12-15 22:26:16 +0100

    added more levels and such

--- phaser/game.js
+++ phaser/game.js
@@ -3,6 +3,8 @@ class Grid {
 		this.cell_size = cell_size
 		this.origin_x = origin_x
 		this.origin_y = origin_y
+		this.base_origin_x = origin_x
+		this.base_origin_y = origin_y
 	}
 }
 
@@ -54,66 +56,70 @@ class Player extends GridObject {
 		super(scene, grid_x, grid_y, grid, texture)
 	}
 
-	// move(move_x, move_y) {
-	// 	this.grid_x += move_x
-	// 	this.grid_y += move_y
-	// }
-
+	// modified ChatGPT code:
 	async move(move_x, move_y) {
-		const target_grid_x = this._grid_x + move_x
-		const target_grid_y = this._grid_y + move_y
+		const target_grid_x = this.grid_x + move_x
+		const target_grid_y = this.grid_y + move_y
 
+		// This should use a function gridToWorld but GridObject.position should too
 		const target_x = target_grid_x * this.grid.cell_size + this.grid.origin_x
 		const target_y = target_grid_y * this.grid.cell_size + this.grid.origin_y
 
-		this.moving = new Promise(resolve => {
+		await new Promise(resolve => {
 			this.scene.tweens.add({
 				targets: this,
 				x: target_x,
 				y: target_y,
-				duration: 150,
-				ease: 'Linear',
+				duration: 550,
+				ease: 'Bounce',
 				onComplete: () => {
-					this._grid_x = target_grid_x
-					this._grid_y = target_grid_y
-					this.moving = null
+					this.grid_x = target_grid_x
+					this.grid_y = target_grid_y
 					resolve()
 				}
 			})
 		})
-
-		await this.moving
 	}
 
 	collide() {
-		console.log("collision :3")
+		// out of range doesn't give an error, but if y is out of bounds the
+		// second index gives an error as the first part is not an array so
+		// the optional chaining operator is used
+		let current_tile = this.scene.level[this.grid_y]?.[this.grid_x]
+		
+		if (current_tile == "B") {
+			this.scene.setLevel(this.scene.level_index + 1)
+		} else if (current_tile == "." || current_tile == undefined) {
+			return -1
+		}
 	}
 }
 
 class GameClass extends Phaser.Scene {
 	preload() {
-		this.load.image('cat', 'assets/cat.png')
+		this.load.image('cat', 'assets/bunny.png')
 		this.load.image('background', 'assets/background.png')
-		this.load.image('tile', 'assets/sushi.png')
-		this.load.image('start', 'assets/wasabi.png')
-		this.load.image('end', 'assets/platform.png')
+		this.load.image('tile', 'assets/tile.png')
+		this.load.image('start', 'assets/lilypad.png')
+		this.load.image('end', 'assets/star.png')
 	}
 
 	create() {
 		// for the html to be able to access runProgram()
 		window.GameClass = this
 
-		this.game_grid = new Grid(100, 540, 810)
-		this.add.image(540, 810, "background")
+		this.game_grid = new Grid(160, 540, 810)
+		this.add.tileSprite(540, 810, 1080, 1620, "background")
 		this.code_area = document.getElementById("code_area")
-		this.level = []
-		
+
 		this.player = new Player(this, 0, 0, this.game_grid, "cat")
+		this.player.setScale(0.8)
 		this.player.setDepth(1)
 
-		this.level_display = this.add.text(16, 16, '', { fontSize: '32px', fill: '#000' })
+		this.level_display = this.add.text(16, 16, '', { fontSize: '100px', fill: '#000' })
 
-		this.setLevel(1)
+		this.level_sprites = []
+		this.setLevel(0)
 	}
 
 	update() {
@@ -121,8 +127,6 @@ class GameClass extends Phaser.Scene {
 	}
 
 	setLevel(level_index) {
-		this.level_display.setText(`Level ${level_index+1}`)
-
 		fetch('levels.txt')
 			.then(r => r.text())
 			.then(text => {
@@ -131,22 +135,34 @@ class GameClass extends Phaser.Scene {
 	}
 
 	loadLevel(levels_string, level_index) {
-		const level = levels_string
+		this.level = levels_string
 			.trim()
 			.split(/\n\s*\n/)[level_index]
-			.trim()
-			.split('\r\n')
-			.map(row => row.split(''))
 
-		const width = level[0].length
-		const height = level.length
+		for (const sprite of this.level_sprites) {
+			sprite.destroy()
+		}
+		this.level_sprites = []
+
+		// win
+		if (this.level == undefined) {
+			this.level_display.setText('You fricking won!!!')
+			return
+		} else {
+			this.level_index = level_index
+			this.level_display.setText(`Level ${level_index + 1}`)
+			this.level = this.level.trim().split('\r\n').map(row => row.split(''))
+		}
+
+		const width = this.level[0].length
+		const height = this.level.length
 
 		// I have no idea why -1/2 makes it centered but it does
-		const x_start = width / 2 - 1/2
-		const y_start = height / 2 - 1/2
+		const x_offset = width / 2 - 1/2
+		const y_offset = height / 2 - 1/2
 
-		this.game_grid.origin_x -= x_start * this.game_grid.cell_size
-		this.game_grid.origin_y -= y_start * this.game_grid.cell_size
+		this.game_grid.origin_x = this.game_grid.base_origin_x - x_offset * this.game_grid.cell_size
+		this.game_grid.origin_y = this.game_grid.base_origin_y - y_offset * this.game_grid.cell_size
 
 		for (let y = 0; y < height; y++) {
 			for (let x = 0; x < width; x++) {
@@ -156,8 +172,8 @@ class GameClass extends Phaser.Scene {
 					B: 'end',
 				}
 
-				let texture = textures[level[y][x]]
-				this.level.push(new GridObject(this, x, y, this.game_grid, texture))
+				let texture = textures[this.level[y][x]]
+				this.level_sprites.push(new GridObject(this, x, y, this.game_grid, texture))
 
 				if (texture == 'start') {
 					this.start_tile = {x: x, y: y}
@@ -192,7 +208,9 @@ class GameClass extends Phaser.Scene {
 				// move_vector is falsy if input not in directions keys
 				if (move_vector) {
 					await this.player.move(move_vector.x, move_vector.y)
-					this.player.collide()
+					if (this.player.collide() == -1) {
+						break
+					}
 				}
 			}
 		}
